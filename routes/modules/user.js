c6169d40355aaab4041c57155af623db2effce2e
tschiang23 <tien23@gmail.com>
2024-10-04 12:40:52 +0800

    feat: add user register feature

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   return res.render('login')
@@ -9,6 +10,28 @@ router.post('/login', (req, res) => { })
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', async (req, res) => {
+  try {
+    const userData = req.body
+    const foundUser = await User.findOne({ email: userData.email }).lean().exec()
+
+    if (foundUser) {
+      console.log('User already exists ')
+      return res.render('register', { userData })
+    }
+
+    if (userData.password !== userData.confirmPassword) {
+      return res.render('register', { userData })
+    }
+
+    delete userData.confirmPassword
+    await User.create({ ...userData })
+    res.redirect('/users/login')
+  } catch (error) {
+    console.log('Error:', error)
+  }
 })
 
 module.exports = router
