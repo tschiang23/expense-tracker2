fd35bc57131e427543d6302d7ea109b62c4c57df
tschiang23 <tien23@gmail.com>
2024-10-04 13:56:59 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('./config/mongoose')
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const routes = require('./routes')
 
 const PORT = process.env.PORT || 3000
@@ -15,6 +16,12 @@ app.set('view engine', 'hbs')
 //middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(routes)
 
 app.listen(PORT, () => console.log(`App is running on ${PORT}`))
