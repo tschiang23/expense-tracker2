c17ad5a169793d2c26feb6171e826e87bc8f2c18
tschiang23 <tien23@gmail.com>
2024-10-04 14:55:49 +0800

    feat: add user authentication in home and expense routes

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const expense = require('./modules/expense')
 const user = require('./modules/user')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/expenses', expense)
 router.use('/users', user)
+
+router.use('/', authenticator, home)
+router.use('/expenses', authenticator, expense)
 
 module.exports = router
 
